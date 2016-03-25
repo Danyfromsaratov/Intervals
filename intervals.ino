baf30299de1ea10460b39924894a1912d04cd318
Danyfromsaratov <danyfromsaratov@yandex.ru>
2016-03-25 10:44:11 +0300

    Задание 1
    Пелипенко Д.Г

--- intervals.ino
+++ intervals.ino
@@ -1,51 +1,99 @@
-const int ledPin = 13;
-const unsigned long interval = 1000;
 
-#define DEBUG
+const int ledPin8 = 8; 
+const int ledPin9 = 9;
+const int ledPin10 = 10;
+const int ledPin12 = 12;
+const int ledPin13 = 13;
+int ledState8 = LOW;            
+int ledState9 = LOW;   
+int ledState10 = LOW;   
+int ledState12 = LOW;   
+int ledState13 = LOW;  
+ 
+unsigned long previousMillis = 0;        
 
-class Interval
-{
-    bool _state;
-    unsigned long _previous;
-    uint8_t _pin;
-    unsigned long _interval;
+// constants won't change :
+const long interval8 = 1000;          
+const long interval9 = 500;  
+const long interval10 = 333;  
+const long interval12 = 250;  
+const long interval13 = 200;
 
-  public:
-    Interval(uint8_t pin, long interval = 1000):
-      _state(false), _previous(0), _pin(pin), _interval(interval)
-    {
-      pinMode(pin, OUTPUT);
-    }
-    void poll(unsigned long);
-    void print();
-};
 
-void Interval::poll(unsigned long current) {
-  if (current - _previous >= _interval) {
-    _previous = current;
-    _state = !_state;
-    digitalWrite(_pin, _state);
-#ifdef DEBUG
-    print();
-#endif
-  }
-}
-
-void Interval::print() {
-  Serial.print("Interval with ");
-  Serial.print(_pin);
-  Serial.print(" pin: ");
-  Serial.println(_state ? "HIGH" : "LOW");
-}
-
-Interval ledInterval(ledPin, interval);
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Intervals example started.");
+  pinMode(ledPin8, OUTPUT);
+   pinMode(ledPin9, OUTPUT);
+    pinMode(ledPin10, OUTPUT);
+     pinMode(ledPin12, OUTPUT);
+      pinMode(ledPin13, OUTPUT);
 }
 
 void loop() {
-  unsigned long current = millis();
-  ledInterval.poll(current);
+  delay(3);
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval8) {
+    previousMillis = currentMillis;
+    
+    if (ledState8 == LOW) {
+      ledState8 = HIGH;
+    } else {
+      ledState8 = LOW;
+    }
+    digitalWrite(ledPin8, ledState8);
+  }
+
+delay(3);
+  unsigned long currentMillis = millis();
+ if (currentMillis - previousMillis >= interval9) {
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState9 == LOW) {
+      ledState9 = HIGH;
+    } else {
+      ledState9 = LOW;
+    }
+    digitalWrite(ledPin9, ledState9);
+  }
+
+  delay(3);
+  unsigned long currentMillis = millis();
+   if (currentMillis - previousMillis >= interval10) {
+    previousMillis = currentMillis;
+    if (ledState10 == LOW) {
+      ledState10 = HIGH;
+    } else {
+      ledState10 = LOW;
+    } 
+    digitalWrite(ledPin10, ledState10);
+  }
+
+delay(3);
+  unsigned long currentMillis = millis();
+   if (currentMillis - previousMillis >= interval12) {
+    previousMillis = currentMillis;
+
+    if (ledState12 == LOW) {
+      ledState12 = HIGH;
+    } else {
+      ledState12 = LOW;
+    }
+    digitalWrite(ledPin12, ledState12);
+  }
+
+  
+delay(3);
+   if (currentMillis - previousMillis >= interval13) {
+    previousMillis = currentMillis;
+    if (ledState13 == LOW) {
+      ledState13 = HIGH;
+    } else {
+      ledState13 = LOW;
+    }
+    digitalWrite(ledPin13, ledState13);
+  }
+  
 }
+
