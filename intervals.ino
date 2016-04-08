6f0da495c12cc3fe6d9b962971ca4cbcff13c130
Danyfromsaratov <danyfromsaratov@yandex.ru>
2016-04-08 10:08:28 +0300

    Задание 3
    Задание 3

--- intervals.ino
+++ intervals.ino
@@ -1,98 +1,54 @@
-const int ledPin1 = 13; 
-const int ledPin2 = 12; 
-const int ledPin3 = 11; 
-const int ledPin4 = 10; 
-const int ledPin5 = 9; 
-
-
-int ledState1 = LOW; 
-int ledState2 = LOW; 
-int ledState3 = LOW; 
-int ledState4 = LOW; 
-int ledState5 = LOW; 
-
-long previousMillis1 = 0; 
-long previousMillis2 = 0; 
-long previousMillis3 = 0; 
-long previousMillis4 = 0; 
-long previousMillis5 = 0; 
-
-long interval1 = 1000; 
-long interval2 = 500; 
-long interval3 = 333; 
-long interval4 = 250; 
-long interval5 = 100; 
-
-
-void setup() { 
-pinMode(ledPin1, OUTPUT); 
-pinMode(ledPin2, OUTPUT); 
-pinMode(ledPin3, OUTPUT); 
-pinMode(ledPin4, OUTPUT); 
-pinMode(ledPin5, OUTPUT); 
+Servo myservo; 
+int echoPin = 9; 
+int trigPin = 8; 
+int cm; 
+void setup() 
+{ 
+myservo.attach(6); 
+pinMode(trigPin, OUTPUT); 
+pinMode(echoPin, INPUT); 
 
 } 
 
-void loop() 
+void servo(int cm) 
 { 
 
-unsigned long currentMillis = millis(); 
-
-//LedPin1 
-if(currentMillis - previousMillis1 > interval1) { 
-// сохраняем время последнего переключения 
-previousMillis1 = currentMillis; 
-
-// если светодиод не горит, то зажигаем, и наоборот 
-if (ledState1 == LOW) 
-ledState1 = HIGH; 
-else 
-ledState1 = LOW; 
-
-// устанавливаем состояния выхода, чтобы включить или выключить светодиод 
-digitalWrite(ledPin1, ledState1); 
+if (cm>=10 && cm<40) 
+{ 
+myservo.write(45); 
 } 
-
-
-//LedPin2 
-if(currentMillis - previousMillis2 > interval2) { 
-previousMillis2 = currentMillis; 
-if (ledState2 == LOW) 
-ledState2 = HIGH; 
-else 
-ledState2 = LOW; 
-digitalWrite(ledPin2, ledState2); 
+else { 
+if (cm>=40 && cm<70) 
+{ 
+myservo.write(90); 
 } 
-
-//LedPin3 
-if(currentMillis - previousMillis3 > interval3) { 
-previousMillis3 = currentMillis; 
-if (ledState3 == LOW) 
-ledState3 = HIGH; 
-else 
-ledState3 = LOW; 
-digitalWrite(ledPin3, ledState3); 
+else { 
+if (cm>=70 && cm<=100 ){ 
+myservo.write(135) ; 
 } 
-
-//LedPin4 
-if(currentMillis - previousMillis4 > interval4) { 
-previousMillis4 = currentMillis; 
-if (ledState4 == LOW) 
-ledState4 = HIGH; 
 else 
-ledState4 = LOW; 
-digitalWrite(ledPin4, ledState4); 
+{ 
+myservo.write(5) ; 
 } 
-
-//LedPin5 
-if(currentMillis - previousMillis1 > interval5) { 
-previousMillis5 = currentMillis; 
-if (ledState5 == LOW) 
-ledState5 = HIGH; 
-else 
-ledState5 = LOW; 
-digitalWrite(ledPin5, ledState5); 
 } 
+}} 
 
+void distans () 
+{ 
+int dist; 
+digitalWrite(trigPin, LOW); 
+delayMicroseconds(2); 
+digitalWrite(trigPin, HIGH); 
+delayMicroseconds(10); 
+digitalWrite(trigPin, LOW); 
+dist = pulseIn(echoPin, HIGH); 
+cm = dist / 58 ; 
+Serial.print(cm); 
+Serial.println(" cm"); 
+servo(cm); 
+} 
+void loop() 
+{ 
+distans(); 
+delay(2000); 
 }
-
